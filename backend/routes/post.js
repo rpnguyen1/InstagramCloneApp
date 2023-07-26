24cf2b1aff909cbbb6ab1434e566c3263c5f36ed
Richard Nguyen <nguyenprichard@gmail.com>
2023-07-26 00:02:16 -0700

    feat: Add delete post functionality, fix related issues
    Implemented the ability to delete posts on the backend by adding a new API route and logic for handling post deletion. Created a route handler for 'DELETE /posts/:postId' to enable authenticated users to delete their own posts.
    
    In the process of testing the delete post functionality, encountered and resolved several related issues:
    
    - Fixed the 'Query.prototype.exec() no longer accepts a callback' error by updating Mongoose code to use promises instead of callbacks. Replaced '.exec()' with '.then()' and '.catch()' to handle asynchronous operations appropriately.
    
    - Addressed the 'TypeError: post.remove is not a function' error by using the 'deleteOne()' method instead of 'remove()' to delete posts. The 'remove()' method was deprecated in Mongoose version 6 and removed in version 6.1.
    
    Additionally, added error handling to provide appropriate responses when a post is not found, when unauthorized users attempt to delete posts, and when encountering errors during the deletion process.
    
    The delete post functionality is now functional, and the related issues have been resolved.

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -124,4 +124,47 @@ router.put("/comment",requireLogin, async (req,res)=>{
 });
 
 
+router.delete('/deletepost/:postId',requireLogin, async (req,res)=>{
+
+    Post.findOne({ _id: req.params.postId })
+    .populate("postedBy", "_id")
+    .then(post => {
+      if (!post) {
+        return res.status(422).json({ error: "Post not found" });
+      }
+  
+      if (post.postedBy._id.toString() === req.user._id.toString()) {
+        return post.deleteOne()
+          .then(result => {
+            res.json(result);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Error removing post" });
+          });
+      } else {
+        res.status(401).json({ error: "Unauthorized to remove post" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error finding post" });
+    });
+    // Post.findOne({_id:req.params.postId})
+    // .populate("postedBy" , "_id")
+    // .exec((err,post)=>{
+    //     if(err || post){
+    //         return res.status(422).json({error:err})
+    //     }
+    //     if(post.postedBy._id.toString() === req.user._id.toString()){
+    //         post.remove()
+    //         .then(result=>{
+    //             res.json(result)
+    //         }).catch(err=>{
+    //             console.log(err)
+    //         })
+    //     }
+    // })
+})
+
 module.exports = router
--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -99,6 +99,23 @@ const Home = ()=>{
         })
     }
 
+
+    const deletePost = (postId)=>{
+        fetch(`http://localhost:5000/deletepost/${postId}`, {
+            method:"delete",
+            headers:{
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+
+            const newData = data.filter(item=>{
+                return item._id !== result._id
+            })
+            setData(newData)
+        })
+    }
     return(
         <div className="home">
             {
@@ -106,7 +123,13 @@ const Home = ()=>{
                 data.map(item=>{
                     return(
                         <div className="card home-card" key={item._id}>
-                            <h5>{item.postedBy.name}</h5>
+                            <h5>{item.postedBy.name} {item.postedBy._id == state._id
+                            && <i className="material-icons" style={{float:"right"}}
+                            onClick={()=>{deletePost(item._id)}}
+                            >delete</i>
+                            }
+                            </h5>
+
                             <div className="card-image">
                                 <img key={item._id} alt="post" src={item.photo}/>
                             </div>
